c8a899aa8231e96d67534efe7a230156f675f495
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-12-07 14:00:17 +0530

    added controller method for fetching all question

--- Backend/quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ Backend/quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.QuestionDetailsResponse;
 import com.upgrad.quora.api.model.QuestionRequest;
 import com.upgrad.quora.api.model.QuestionResponse;
 import com.upgrad.quora.service.business.QuestionBusiness;
@@ -10,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/")
@@ -25,5 +29,21 @@ public class QuestionController {
         final QuestionEntity createdQuestionEntity = questionBusiness.createQuestion(questionEntity, authorization.split("Bearer ")[1]);
         QuestionResponse questionResponse = new QuestionResponse().id(createdQuestionEntity.getUuid()).status("QUESTION CREATED");
         return new ResponseEntity<>(questionResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/question/all")
+    private ResponseEntity<List<QuestionDetailsResponse>> getAllQuestions(@RequestHeader("authorization") final String authorization) throws AuthorizationFailedException {
+        List<QuestionEntity> list = questionBusiness.getAllQuestion(authorization.split("Bearer ")[1]);
+
+        List<QuestionDetailsResponse> questions = list.stream()
+                .map(question -> {
+                    QuestionDetailsResponse q = new QuestionDetailsResponse();
+                    q.setContent(question.getContent());
+                    q.setTitle(question.getTitle());
+                    q.setId(question.getUuid());
+                    return q;
+                }).collect(Collectors.toList());
+
+        return new ResponseEntity<>(questions, HttpStatus.OK);
     }
 }
