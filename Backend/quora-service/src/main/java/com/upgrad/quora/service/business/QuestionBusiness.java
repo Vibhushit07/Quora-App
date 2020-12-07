9416d8b61c939efc4e2e26ad94aac2890f8737f4
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-12-07 13:13:02 +0530

    added service method for posting a question

---
+++ Backend/quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusiness.java
@@ -0,0 +1,45 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Service
+public class QuestionBusiness {
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String authToken) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userDao.getUserAuthByAccessToken(authToken);
+
+        if(userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else {
+            if(userAuthEntity.getLogoutAt() != null) {
+                throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post a question");
+            } else {
+                UserEntity userEntity = userAuthEntity.getUser();
+
+                questionEntity.setDate(ZonedDateTime.now());
+                questionEntity.setUuid(UUID.randomUUID().toString());
+                questionEntity.setUser(userEntity);
+
+                return questionDao.addQuestion(questionEntity);
+            }
+        }
+    }
+}
