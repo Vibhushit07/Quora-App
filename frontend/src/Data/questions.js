65d674b5ea2d501a6f9e714df4b7dfd4c432e983
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-12-07 23:28:20 +0530

    stored data of questions

---
+++ frontend/src/Data/questions.js
@@ -0,0 +1,10 @@
+var questions = [];
+var userQuestions = [];
+
+export const getQuestions = () => questions;
+
+export const setQuestions = (question) => questions = question;
+
+export const getUserQuestion = () => userQuestions;
+
+export const setUserQuestion = (question) => userQuestions = question;
