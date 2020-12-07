4da4c74734e662f4f59fd097a1b8f3e680d37942
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-12-07 23:26:11 +0530

    integrated api for fetching all questions of a user

--- frontend/src/Services/question.js
+++ frontend/src/Services/question.js
@@ -1,4 +1,4 @@
-import { getHeader } from '../Data/userData';
+import { getHeader, getUserData } from '../Data/userData';
 
 export const postQuestion = async (question) => {
 
@@ -51,6 +51,21 @@ export const deleteQuestion = async (questionId) => {
 
     return await fetch('/api/question/delete/' + questionId, {
         method: 'delete',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': getHeader().authorization
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log('error ', err));
+}
+
+export const getUserQuestions = async () => {
+
+    return await fetch('/api/question/all/' + getUserData().userId, {
+        method: 'get',
         headers: {
             'Content-Type': 'application/json',
             'authorization': getHeader().authorization
