725ad5d18d3a1ebb1a7b4505959552cd6d7be59f
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-12-17 18:48:45 +0530

    added routing in user questions and styling in user questions

--- frontend/src/Routes/user/userQuestions.jsx
+++ frontend/src/Routes/user/userQuestions.jsx
@@ -2,17 +2,41 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 import { Edit, DeleteForever } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { DialogBox } from "../../Components/dialogBox";
 import { setDialogBox } from "../../Data/dialogBox";
 import { deleteQuestion, getUserQuestions } from "../../Services/question";
 import { getUserQuestion, setUserQuestion } from "../../Data/questions";
+import { setAnswers, setQuestionId } from "../../Data/answers";
+import { getAnswersOfQuestion } from "../../Services/answer";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: "4vw",
+    padding: "2vw",
+    width: "90%",
+  },
+  hr: {
+    width: "95%",
+  },
+  title: {
+    cursor: "pointer",
+  },
+}));
 
 export const UserQuestions = () => {
   const [open, setOpen] = React.useState(false);
   const [questions, setQuestions] = React.useState(getUserQuestion());
   const history = useHistory();
+  const classes = useStyles();
   const [questionId, setQuestionId] = React.useState("");
+
+  const displayAnswers = async (questionId) => {
+    setQuestionId(questionId);
+    await setAnswers(await getAnswersOfQuestion());
+    history.push("/answer/get");
+  };
 
   const handleClick = (id) => {
     console.log("delete");
@@ -41,19 +65,30 @@ export const UserQuestions = () => {
   };
 
   return (
-    <div>
-      {questions.map((question) => (
-        <div>
-          <span onClick={() => history.push("/question/edit/" + question.id)}>
-            <Edit />
-          </span>
-          <span onClick={() => handleClick(question.id)}>
-            <DeleteForever />
-          </span>
-          <h1>{question.title}</h1>
-          <p>{question.content}</p>
-        </div>
-      ))}
+    <div className={classes.container}>
+      {questions.length === 0 ? (
+        <h2 className={classes.title}>You have not posted any questions</h2>
+      ) : (
+        questions.map((question, index) => (
+          <div>
+            <span onClick={() => history.push("/question/edit/" + question.id)}>
+              <Edit />
+            </span>
+            <span onClick={() => handleClick(question.id)}>
+              <DeleteForever />
+            </span>
+            <h1
+              className={classes.title}
+              onClick={() => displayAnswers(question.id)}
+              title="View Answers"
+            >
+              {index + 1}. {question.title}
+            </h1>
+            <p>{question.content}</p>
+            <hr className={classes.hr} />
+          </div>
+        ))
+      )}
       <DialogBox open={open} accept={handleAccept} reject={handleReject} />
     </div>
   );
